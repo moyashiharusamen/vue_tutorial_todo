966a8699c5ca2d12b1d7a03eae44b43d93d71747
moyashiharusamen <moyashi.harusamen@gmail.com>
2020-04-16 22:38:17 +0900

    ローカルストレージ API の使用

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+// https://jp.vuejs.org/v2/examples/todomvc.html
+const STORAGE_KEY = 'todos-vuejs-demo'
+let todoStorage = {
+    fetch() {
+        const todos = JSON.parse(
+            localStorage.getItem(STORAGE_KEY) || '[]'
+        )
+
+        todos.forEach((todo, index) => {
+            todo.id = index
+        })
+        todoStorage.uid = todos.length
+
+        return todos
+    },
+    save(todos) {
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+    }
+}
+
 const app = new Vue({
     el: '#app',
     data: {
